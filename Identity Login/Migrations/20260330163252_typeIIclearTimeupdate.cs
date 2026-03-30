407b0c2a1ea22ca3220bfaeb5592003d50cd84dc
mehta <mehtabatkips@gmail.com>
2026-03-30 21:36:35 +0500

    pdf 4 pages tested

---
+++ Identity Login/Migrations/20260330163252_typeIIclearTimeupdate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity_Login.Migrations
+{
+    /// <inheritdoc />
+    public partial class typeIIclearTimeupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "classifications",
+                keyColumn: "ClassificationId",
+                keyValue: 2,
+                column: "Minutes",
+                value: 27);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "classifications",
+                keyColumn: "ClassificationId",
+                keyValue: 2,
+                column: "Minutes",
+                value: 18);
+        }
+    }
+}
